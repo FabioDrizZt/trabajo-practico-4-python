fef6bf9ba23b63b448a82db0af8da19164725801
ing.fabio.arg <ing.fabio.arg@fi.unju.edu.ar>
2024-06-13 20:48:59 +0000

    ej4

--- ej4.py
+++ ej4.py
@@ -5,3 +5,12 @@ datos = [20, 14]
 # Ejemplo de salida esperada
 # Para 20: [20, 18, 16, ..., 2]
 # Para 14: [14, 12, 10, ..., 2]
+
+def paresOrdenDescendente(a):
+    lista = []
+    for i in range(20, 1, -2):
+        lista.append(i)
+    return lista
+
+for i in datos:
+    print(paresOrdenDescendente(i))
